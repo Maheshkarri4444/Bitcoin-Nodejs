e723a43f2dcff559bffe00dc39f4ecb26902e595
Maheshkarri4444 <maheshkarri2109@gmail.com>
2025-07-16 23:08:37 +0530

    added difficulty&minerate

--- dev-test.js
+++ dev-test.js
@@ -1,8 +1,16 @@
-const Block = require("./blockchain/block");
+// const Block = require("./blockchain/block");
 
-// const block = new Block('hi','this','is','mahesh');
-// console.log(block.toString());
-// console.log(Block.genesis().toString());
+// // const block = new Block('hi','this','is','mahesh');
+// // console.log(block.toString());
+// // console.log(Block.genesis().toString());
 
-const fooBlock = Block.mineBlock(Block.genesis(),'foo');
-console.log(fooBlock.toString());
+// const fooBlock = Block.mineBlock(Block.genesis(),'foo');
+// console.log(fooBlock.toString());
+
+const Blockchain = require('./blockchain');
+
+const bc = new Blockchain();
+
+for ( let i = 0; i<10;i++){
+    console.log(bc.addBlock(`block ${i}`).toString())
+}
