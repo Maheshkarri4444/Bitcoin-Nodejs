2cd0cbaef86261f8c043eb4b9c85f4d96113c7ac
Maheshkarri4444 <maheshkarri2109@gmail.com>
2025-07-16 19:06:46 +0530

    connecting to peers testing

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -3,6 +3,12 @@ const Websocket = require("ws");
 const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(','):[];
 
+//for testing run this in 2nd terminal  HTTP_PORT=3002 P2P_PORT=5002 PEERS=ws://localhost:5001 npm run dev
+//in 3rd terminal HTTP_PORT=3003 P2P_PORT=5003 PEERS=ws://localhost:5001,ws://localhost:5002 npm run dev
+//in 4th terminal HTTP_PORT=3004 P2P_PORT=5004 PEERS=ws://localhost:5001,ws://localhost:5002,ws://localhost:5003 npm run dev
+
+
+
 
 class P2pServer {
     constructor(blockchain){
@@ -13,6 +19,19 @@ class P2pServer {
     listen(){
         const server = new Websocket.Server({port:P2P_PORT});
         server.on('connection',socket=> this.connectSocket(socket));
+
+        this.connectToPeers();
+        
+        console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
+    }
+
+    connectToPeers(){
+        peers.forEach(peer => {
+            
+            const socket = new Websocket(peer);
+
+            socket.on('open',()=>this.connectSocket(socket));
+        });
     }
 
     connectSocket(socket){
@@ -20,3 +39,5 @@ class P2pServer {
         console.log("socket Connected")
     }
 }
+
+module.exports = P2pServer;
