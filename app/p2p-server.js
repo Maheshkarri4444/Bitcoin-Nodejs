810e63e0d9c4e0c7822df55d9c102c10b8fc81c5
Maheshkarri4444 <maheshkarri2109@gmail.com>
2025-07-16 19:59:03 +0530

    handling data sharing from peers (not synchronization)

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -37,6 +37,16 @@ class P2pServer {
     connectSocket(socket){
         this.sockets.push(socket);
         console.log("socket Connected")
+
+        this.messageHandler(socket);
+        socket.send(JSON.stringify(this.blockchain.chain));
+    }
+
+    messageHandler(socket){
+        socket.on('message',message=>{
+            const data = JSON.parse(message);
+            console.log("data: ",data);
+        })
     }
 }
 
