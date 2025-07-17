79c109a8bcdb457f307a1d7bda4b64bbef7ed5cf
Maheshkarri4444 <maheshkarri2109@gmail.com>
2025-07-17 09:13:51 +0530

    keypair generation added and tested

--- dev-test.js
+++ dev-test.js
@@ -7,10 +7,14 @@
 // const fooBlock = Block.mineBlock(Block.genesis(),'foo');
 // console.log(fooBlock.toString());
 
-const Blockchain = require('./blockchain');
+// const Blockchain = require('./blockchain');
 
-const bc = new Blockchain();
+// const bc = new Blockchain();
 
-for ( let i = 0; i<10;i++){
-    console.log(bc.addBlock(`block ${i}`).toString())
-}
+// for ( let i = 0; i<10;i++){
+//     console.log(bc.addBlock(`block ${i}`).toString())
+// }
+
+const Wallet = require('./wallet');
+const wallet = new Wallet();
+console.log(wallet.toString());
